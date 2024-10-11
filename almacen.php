a08aa1301ab703652153bbb383e7202174382849
maralde <marcosallet@gmail.com>
2024-10-11 14:02:26 +0200

    añadido la pantalla almacen

--- almacen.php
+++ almacen.php
@@ -1,6 +1,34 @@
 <?php
 require_once('./configure/configure.php');
 
+$sql='SELECT h.Id_producto AS Id, p.Nombre_prod AS nombre, SUM(h.Cantidad_compra)AS cantidad, AVG(h.Precio_compra) AS precio, (Sum(h.Cantidad_compra) * AVG(h.Precio_compra)) AS precioTotal FROM historial AS h INNER JOIN producto AS p ON h.Id_producto = p.Id_producto GROUP BY h.Id_producto, p.Id_producto';
+
+$result = $conn->query($sql);
+$tabla = "";
+if($result -> num_rows > 0){
+    $tabla = "<table class='table table-bordered'>
+    <thead>
+        <tr>
+            <th> ID </th>
+            <th> Nombre </th>
+            <th> Cantidad KG </th>
+            <th> Precio Medio € </th>
+            <th> Precio total € </th>
+        </tr>
+    </thead>
+    <tbody>";
+    while($row = $result -> fetch_assoc()){
+        $tabla .= "<tr>
+        <td>".$row["Id"]."</td>
+        <td>".$row["nombre"]."</td>
+        <td>".$row["cantidad"]."</td>
+        <td>".round($row["precio"],2)."</td>
+        <td>".round($row["precioTotal"],2)."</td>
+        </tr>";
+    }
+    $tabla .="</tbody></table>";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -36,6 +64,10 @@ require_once('./configure/configure.php');
 </head>
 <body>
     <?php echo $nav; ?>
+    <div class="container mt-5">
+
+    <?php echo $tabla; ?>
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
